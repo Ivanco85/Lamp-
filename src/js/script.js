1654dd0054c89303d6d9b9ba2b6e152224657b76
Ivanco <ivan.wasilev21@gmail.com>
2022-12-20 18:04:17 +0300

    class js

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,97 @@
-const lamp = document.querySelector('.lamp')
-const button = document.querySelector('.button')
+class Lamp {
+    light
+    button
+    brightles = 1000
+    onOff = false
 
-button.addEventListener('click', buttonClickHandler)
-
-function buttonClickHandler(event) {
-    event.target.classList.toggle('button-off')
-    event.target.classList.toggle('button-on')
-    if (event.target.classList[1] === 'button-off') {
-        console.log('Кнопка отжата')
+    constructor(light, button) {
+        this.light = light
+        this.button = button
+        this.button.addEventListener('click', () => {
+            this.lampToggle()
+        })
+        this.light.addEventListener('mousewheel', (event) => {
+            this.lampBrigtlesToggle(event)
+        })
     }
-    else {
-        console.log('Кнопка нажата')
+
+    lampToggle() {
+        console.log('contecst ', this)
+        this.button.classList.toggle('button-off')
+        this.button.classList.toggle('button-on')
+        this.light.classList.toggle('lamp-off')
+        this.light.classList.toggle('lamp-on')
+        if (this.onOff === false) {
+            this.onOff = true
+        }
+        else {
+            this.onOff = false
+            this.brightles = 1000
+            this.light.style.opacity = 1
+        }
     }
 
-    lamp.classList.toggle('lamp-off')
-    lamp.classList.toggle('lamp-on')
+    lampBrigtlesToggle(event) {
+        if (this.onOff === true) {
+            this.brightles = this.brightles + event.deltaY;
+            if (this.brightles > 1000) {
+                this.brightles = 1000
+            }
+            if (this.brightles < 100) {
+                this.brightles = 100
+            }
+            const result = this.brightles / 1000
+            this.light.style.opacity = result
+        }
+    }
+
 }
+
+const lamp = new Lamp(document.querySelector('.lamp'), document.querySelector('.button'))
+
+
+// const lamp = {
+//     ligth: document.querySelector('.lamp'),
+//     button: document.querySelector('.button'),
+
+//     brightles: 1000,
+//     onOff: false,
+
+//     lampToggle: function () {
+//         lamp.button.classList.toggle('button-off')
+//         lamp.button.classList.toggle('button-on')
+//         lamp.ligth.classList.toggle('lamp-off')
+//         lamp.ligth.classList.toggle('lamp-on')
+//         if (lamp.onOff === false) {
+//             lamp.onOff = true
+//         }
+//         else {
+//             lamp.onOff = false
+//             lamp.brightles = 1000
+//             lamp.ligth.style.opacity = 1
+
+//         }
+
+//     },
+//     lampBrigtlesToggle: function (event) {
+//         if (lamp.onOff === true) {
+//             lamp.brightles = lamp.brightles + event.deltaY;
+//             if (lamp.brightles > 1000) {
+//                 lamp.brightles = 1000
+//             }
+//             if (lamp.brightles < 100) {
+//                 lamp.brightles = 100
+//             }
+//             result = lamp.brightles / 1000
+//             lamp.ligth.style.opacity = result
+//             console.log(result);
+//         }
+//     }
+// }
+
+
+// lamp.button.addEventListener('click', lamp.lampToggle)
+
+// lamp.ligth.addEventListener('mousewheel', lamp.lampBrigtlesToggle)
+
 
